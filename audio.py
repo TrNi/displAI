a56590dd6222e20bafa4506ac091291c9d6e2aa7
Vanderson Martins do Rosario <vandersonmr2@gmail.com>
2023-04-16 20:31:30 -0700

    Improving conversation

--- audio.py
+++ audio.py
@@ -9,7 +9,9 @@ from threading import Thread
 import time
 import tkinter as tk
 from PIL import Image, ImageTk
+from multiprocessing import Process, Value
 import threading
+import numpy as np
 
 class GifViewer(threading.Thread):
     def __init__(self, gif_files):
@@ -30,7 +32,7 @@ class GifViewer(threading.Thread):
         self.tk.configure(background='black')
 
         self.tk.label = tk.Label(self.tk)
-        self.tk.label.pack(padx=10, pady=10)
+        self.tk.label.pack(padx=400, pady=400)
 
         self.load_gif()
         self.tk.mainloop()
@@ -78,38 +80,42 @@ def get_embedding(msg):
     resp = openai.Embedding.create(
         input=msg,
         engine="text-similarity-davinci-001")
+    return resp['data'][0]['embedding']
 
 def similarity(emb1, emb2):
     return np.dot(emb1, emb2)
 
-def process():
-    global conversation
+def update_history():
+    global conversation, history, history_embeddings
     while True:
-        time.sleep(1)
+        time.sleep(30)
         for i in range(len(conversation)):
-            if i > 5:
-                print("Starting processing conversation:")
-                old_conversation = conversation[i:len(conversation)]
+            if i > 10:
+                old_conversation = conversation[0:i]
                 history += old_conversation
                 history_embeddings += [get_embedding(msg) for msg in old_conversation]
-                conversation = conversation[0:i]
+                conversation = conversation[i:len(conversation)+1]
                 break
 
 def get_context(msg):
+    global history_embeddings, history
     similarities = []
-    msg_emb = get_embedding(msg)
+    msg_emb = get_embedding(msg.strip(user_name).strip("sophia"))
     for emb, msg in zip(history_embeddings, history):
-        similarities += [(similarity(emb, msg), msg)]
+        similarities += [(similarity(emb, msg_emb.strip(user_name).strip("sophia")), msg)]
     similarities.sort()
     context = []
     for sim, msg in reversed(similarities):
         context += [msg]
-        if len(context) > 3:
+        if len(context) > 5:
             break
     return context
 
+sophia_awake = False
+sophia_time = time.time()
+
 def callback(recognizer, audio):
-    global conversation, user_name, oai
+    global sophia_awake, sophia_time, conversation, user_name, oai
     try:
         print("History: ")
         print(history)
@@ -117,21 +123,31 @@ def callback(recognizer, audio):
         print(conversation)
         print("Starting transcription...")
         app.show_gif(4)
-        user_text = recognizer.recognize_whisper(audio, model = "small.en")
+        user_text = recognizer.recognize_whisper(audio, model = "base.en")
         print(f"Recognized: {user_text}")
 
-        if user_text and ("sophia" in user_text.lower() or "sofia" in user_text.lower()):
-            app.show_gif(2)
+        if user_text:
+            if ("sophia" in user_text.lower() or "sofia" in user_text.lower()):
+                sophia_awake = True
+                sophia_time = time.time()
+            elif sophia_awake and time.time() - sophia_time < 20:
+                print(time.time() - sophia_time)
+                sophia_time = time.time()
+            else:
+                sophia_awake = False
+                app.show_gif(2)
+                return
+
             user_text = user_text.strip()
-            conversation += [user_name + ": " + user_text.strip("hey sophia")]
-            context = get_context(user_text.strip("hey sophia"))
+            conversation += [user_name + ": " + user_text.strip("sophia:")]
+            context = get_context(user_text.strip("sophia:"))
             print("Context: ")
             print(context)
 
-            prompt = "Pretend being a very smart girl, be very friendly and give short anwsers to generate the next line from Sophia to " + user_name + " for this conversation: \n" + '\n'.join(context) + '\n'.join(conversation)
+            prompt = "You are Sophia, a very smart AI. Be very friendly and create the next awnser from Sophia to " + user_name + " for this conversation: \n" + '\n'.join(context) + '\n'.join(conversation)
             print(prompt)
             chatgpt_text = oai.chat_completion(prompt = prompt)
-            chatgpt_text = chatgpt_text.strip("sophia:").strip("Sophia:")
+            chatgpt_text = chatgpt_text.strip("sophia:").strip("Sophia:").split('\n')[0]
 
             conversation += ["sophia: " + chatgpt_text]
 
@@ -148,19 +164,26 @@ def callback(recognizer, audio):
 app = GifViewer(gif_files)
 
 def main():
+    global sophia_awake
     r = sr.Recognizer()
     mic = sr.Microphone()
     with mic as source:
         r.adjust_for_ambient_noise(source)
+    r.pause_threshold = 1
+    r.operation_timeout = 0.7
+    r.dynamic_energy_threshold = True
 
-    processThread = Thread(target=process)
+    processThread = threading.Thread(target=update_history)
     processThread.start()
 
     while True:
-        app.show_gif(0)
+        if sophia_awake:
+            app.show_gif(0)
+        else:
+            app.show_gif(2)
         print("Listening...")
         with mic as source:
-            audio = r.listen(source, timeout=5)
+            audio = r.listen(source, timeout=3)
         print("Listened")
         callback(r, audio)
 
